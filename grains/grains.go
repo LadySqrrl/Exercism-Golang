99caae2cd7c5c9099a988c1a971fb97e3a50a33a
LadySqrrl <72673087+LadySqrrl@users.noreply.github.com>
2021-05-06 14:00:47 -0500

    fixed grains

--- grains/grains.go
+++ grains/grains.go
@@ -5,32 +5,16 @@ import "errors"
 
 //Square returns the number of grains on a given square
 func Square(input int) (uint64, error) {
-	previousSquare := 1
-	currentSquare := 1
 
 	if input < 1 || input > 64 {
 		return uint64(0), errors.New("out of bounds")
 	}
 
-	for i := 1; i < input; i++ {
-		currentSquare = previousSquare * 2
-		previousSquare = currentSquare
-	}
-
-	return uint64(currentSquare), nil
+	return 1 << (input - 1), nil
 }
 
 //Total returns the total number of grains on the board
 func Total() uint64 {
-	previousSquare := 1
-	currentSquare := 1
-	total := 1
 
-	for i := 1; i < 65; i++ {
-		currentSquare = previousSquare * 2
-		previousSquare = currentSquare
-		total += currentSquare
-	}
-
-	return uint64(total)
+	return 1<<64 - 1
 }
